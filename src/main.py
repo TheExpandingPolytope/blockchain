1f78a07a328369f9a62e23f5e1b1ad40dd349028
Jesse Engerman <jesseengerman@hotmail.com>
2020-06-10 14:37:48 -0500

    block chain class

--- src/main.py
+++ src/main.py
@@ -10,6 +10,16 @@ class block:
         self.time = time
         self.prev_hash = prev_hash
 
-    def hash(data):
+    def get_hash(self):
         block_string = json.dumps(self.__dict__, sort_keys=True) # The string equivalent also considers the previous_hash field now
         return sha256(block_string.encode()).hexdigest()
+
+class chain:
+    def __init__(self):
+        self.chain = []
+        self.genesis_block()
+
+    def genesis_block(self):
+        gen_block = block(0, [], time.time(), "0")
+        gen_block.hash = gen_block.get_hash()
+    
