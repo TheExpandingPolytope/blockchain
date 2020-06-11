cc2fa214d18aa5071422519822067929ac204644
Jesse Engerman <jesseengerman@hotmail.com>
2020-06-11 17:41:02 -0500

    read me

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from hashlib import sha256
 import json
 import time
 
-
 class block:
     def __init__(self, index, data, time, prev_hash):
         self.index = index
@@ -17,9 +16,7 @@ class block:
 class chain:
     def __init__(self):
         self.chain = []
-        self.genesis_block()
-
-    def genesis_block(self):
         gen_block = block(0, [], time.time(), "0")
         gen_block.hash = gen_block.get_hash()
-    
+        self.chain.append(gen_block)
+        
